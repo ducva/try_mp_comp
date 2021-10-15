feb2281d60efb3ccc9e08544144c548d6d92eb21
Michael <vuanhduc2685@gmail>
2021-10-15 22:44:02 +0700

    add tailwind and config storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,21 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   "core": {
     "builder": "webpack5"
