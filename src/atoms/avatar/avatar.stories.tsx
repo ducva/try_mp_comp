1ac371d90cc579d7b539641f6def3b70b3a5f2d2
Michael <vuanhduc2685@gmail>
2021-10-19 11:15:47 +0700

    update

--- src/atoms/avatar/avatar.stories.tsx
+++ src/atoms/avatar/avatar.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Story, ComponentMeta } from '@storybook/react';
-import Avatar, {AvatarProps} from "./avatar";
+import {ComponentMeta, Story} from '@storybook/react';
+import Avatar, {AvatarProps, AvatarShape, AvatarSize} from "./avatar";
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -16,5 +16,7 @@ const Template: Story<AvatarProps> = (args) => <Avatar {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-    src: 'https://via.placeholder.com/50'
+    src: 'https://via.placeholder.com/50',
+    shape: AvatarShape.rounded,
+    size: AvatarSize.regular,
 };
--- src/atoms/avatar/avatar.tsx
+++ src/atoms/avatar/avatar.tsx
@@ -5,6 +5,11 @@ export enum AvatarShape {
     "rounded" = "rounded",
     "circular" = "circular"
 }
+export enum AvatarSize {
+    "small" = "small",
+    "regular" = "regular",
+    "large" = "large"
+}
 export type AvatarProps = {
     /**
      * Image source
@@ -13,7 +18,7 @@ export type AvatarProps = {
     /**
      * Size
      */
-    size?: "small" | "regular" | "large";
+    size?: AvatarSize,
     /**
      * Shape
      */
@@ -22,11 +27,14 @@ export type AvatarProps = {
      * alt text
      */
     alt?: string;
+
+    hasNotification?: boolean;
+    notificationPosition?: "top" | 'bottom';
 } & React.HTMLAttributes<HTMLDivElement>
 
 const theme = {
     avatar: {
-        base: 'inline-block relative',
+        base: 'inline-block relative overflow-hidden',
         size: {
             small:  'h-6 w-6',
             regular: 'h-8 w-8',
@@ -37,18 +45,25 @@ const theme = {
             circular: 'rounded-full'
         },
         placeholder: {
-            base: "bg-gray-100 overflow-hidden"
+            base: "bg-gray-100"
+        },
+        notification: {
+            base: 'absolute block rounded-full ring-2 ring-white bg-green-400 h-2.5 w-2.5',
+            position: {
+                top: 'top-0 right-0 ',
+                bottom: 'bottom-0 right-0'
+            }
         }
     }
 }
 export const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(function Avatar(props, ref) {
-    const {size = 'regular', shape = AvatarShape.rounded, src, alt, className, ...other} = props
+    const {size = AvatarSize.regular, shape = AvatarShape.rounded, src, alt, className, hasNotification=false, notificationPosition="top", ...other} = props
     const {avatar} = theme;
     const baseStyle = avatar.base
     const sizeStyles = {
-        large: avatar.size.large,
-        regular: avatar.size.regular,
-        small: avatar.size.small,
+        [AvatarSize.large]: avatar.size.large,
+        [AvatarSize.regular]: avatar.size.regular,
+        [AvatarSize.small]: avatar.size.small,
     }
 
     const shapeStyles = {
@@ -56,27 +71,28 @@ export const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>(function Ava
         [AvatarShape.circular]: avatar.shape.circular
     }
 
-    const cls = classNames(baseStyle, sizeStyles[size], className)
-    const imageCls = classNames("object-cover", shapeStyles[shape])
-    const placeholderCls = classNames(baseStyle, avatar.placeholder.base, sizeStyles[size], shapeStyles[shape])
+    const notificationPositionStyles = {
+        top: avatar.notification.position.top,
+        bottom: avatar.notification.position.bottom
+    }
 
+    const cls = classNames(baseStyle, sizeStyles[size], shapeStyles[shape], className, !src && avatar.placeholder.base)
+    const imageCls = classNames("object-cover", shapeStyles[shape])
+    const notificationCls = classNames(avatar.notification.base, notificationPositionStyles[notificationPosition] )
     return (
-        <>
-        {src && (
-            <div className={cls} ref={ref} {...other}>
-                {src && (
-                    <img className={imageCls} src={src} alt={alt} loading="lazy"/>
-                )}
-            </div>
-        )}
-        {!src && (
-            <span className={placeholderCls}>
+        <span className={cls} ref={ref} {...other}>
+            {src && (
+                <img className={imageCls} src={src} alt={alt} loading="lazy"/>
+            )}
+            {!src && (
                 <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                 </svg>
-            </span>
-        )}
-        </>
+            )}
+            {hasNotification && (
+                <span className={notificationCls} />
+            )}
+        </span>
     )
 })
 export default Avatar;
